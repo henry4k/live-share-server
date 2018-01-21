76ed308b6cde8554bad851b003382ddb2104d8a7
Henry Kielmann <henrykielmann@gmail.com>
2018-01-21 20:47:37 +0100

    Initial documentation

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "live-share",
+  "version": "0.1.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-01-21T19:38:08.309Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
