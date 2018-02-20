2531a7d73da1982579c24d0f716b7db91ef9105e
Henry Kielmann <henrykielmann@gmail.com>
2018-02-20 12:06:35 +0100

    .

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -4,35 +4,58 @@ const webpack = require('webpack');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const NodeExternals = require('webpack-node-externals');
+const AutoDllPlugin = require('autodll-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 function relativePath(sub) {
-    return path.join(__dirname, sub);
+    return path.resolve(__dirname, sub);
 }
 
+const nodeLibs = ['rxjs', 'immutable'];
 const outputPath = relativePath('dist');
+const generateSourceMaps = true;
+const minimize = true;
+
+const minimizePlugins = [];
+if(minimize)
+    minimizePlugins.push(new webpack.optimize.UglifyJsPlugin({
+        sourceMap: generateSourceMaps
+    }));
+
+const extractCssPlugin = new ExtractTextPlugin({
+    filename: 'style.css'
+});
 
 module.exports = function(env) {
-    const generateSourceMaps = true;
     return [{
+        context: path.resolve(__dirname),
         target: 'web',
         node: false,
         devtool: generateSourceMaps ? 'source-map' : '',
-        externals: [NodeExternals({
-            whitelist: [/^rxjs/]
-        })],
+        watchOptions: {
+            ignored: relativePath('node_modules')
+        },
+        externals: [
+            NodeExternals({
+                whitelist: nodeLibs.map(name => new RegExp('^'+name))
+            })
+        ],
         entry: [
             relativePath('src/script/main.js'),
-            relativePath('src/index.html')
+            relativePath('src/index.html'),
+            relativePath('src/style.scss')
         ],
         output: {
             path: outputPath,
             filename: 'script.js'
+            //publicPath: '/'
         },
         module: {
             rules: [
                 {
                     test: [/\.js$/],
                     loader: 'babel-loader',
+                    include: relativePath('src/script'),
                     options: {
                         presets: [['env', {
                             targets: {
@@ -47,7 +70,7 @@ module.exports = function(env) {
                         {
                             loader: 'file-loader',
                             options: {
-                                name: '[name].html'
+                                name: '[name].[ext]'
                             }
                         },
                         {
@@ -56,9 +79,9 @@ module.exports = function(env) {
                         {
                             loader: 'html-loader',
                             options: {
+                                minimize: minimize,
                                 attrs: [
-                                    'img:src',
-                                    'link:href'
+                                    'img:src'
                                 ]
                             }
                         }
@@ -66,35 +89,44 @@ module.exports = function(env) {
                 },
                 {
                     test: [/\.scss$/],
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[name].css'
+                    use: extractCssPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: generateSourceMaps,
+                                    minimize: minimize
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: generateSourceMaps
+                                }
                             }
-                        },
-                        {
-                            loader: 'extract-loader'
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: generateSourceMaps,
-                                minimize: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: generateSourceMaps
-                            }
-                        }
-                    ]
+                        ]
+                    })
                 }
             ]
         },
         plugins: [
-            new CleanWebpackPlugin([outputPath])
-        ]
+            new CleanWebpackPlugin([outputPath]),
+            extractCssPlugin,
+            new AutoDllPlugin({
+                filename: '[name].js',
+                entry: {
+                    vendor: nodeLibs
+                },
+                plugins: minimizePlugins,
+                inherit: function(mainConfig) {
+                    const config = Object.assign({}, mainConfig);
+                    delete config.entry;
+                    delete config.output;
+                    delete config.plugins;
+                    return config;
+                }
+            })
+        ].concat(minimizePlugins)
     }];
 };
